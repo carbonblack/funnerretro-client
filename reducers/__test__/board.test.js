2f968d4f263f24706f8d60bddf91234ddabe7e12
Jake Hackett <jhackett@carbonblack.com>
2018-05-21 09:00:10 -0400

    adding more reducer tests

--- reducers/__test__/board.test.js
+++ reducers/__test__/board.test.js
@@ -38,4 +38,176 @@ describe('board reducers', () => {
             ]
         })
     })
+
+    test('should update column with given id to have new information', () => {
+        expect(reducer({
+            columns: [
+                {
+                    id: 'hello',
+                    content: {
+                        name: 'name'
+                    },
+                    parent: 'hello-parent',
+                    child: 'hello-child'
+                },
+                {
+                    id: 'hellothere',
+                    content: {
+                        name: 'other name'
+                    },
+                    parent: 'hellothere-parent',
+                    child: 'hellothere-child'
+                }
+            ]
+        }, {
+            type: actions.UPDATE_COLUMN,
+            column: {
+                id: 'hello',
+                content: {
+                    name: 'hello changed name'
+                },
+                parent: 'hello-parent-changed',
+                child: 'hello-child-changed'
+            }
+        })).toEqual({
+            columns: [
+                {
+                    id: 'hello',
+                    content: {
+                        name: 'hello changed name'
+                    },
+                    parent: 'hello-parent-changed',
+                    child: 'hello-child-changed'
+                },
+                {
+                    id: 'hellothere',
+                    content: {
+                        name: 'other name'
+                    },
+                    parent: 'hellothere-parent',
+                    child: 'hellothere-child'
+                }
+            ]
+        })
+    })
+
+    test('should reset board content if the board is deleted', () => {
+        expect(reducer({
+            id: 'board-id',
+            content: {
+                name: 'hello'
+            },
+            columns: [
+                {
+                    id: 'hellothere',
+                    content: {
+                        name: 'other name'
+                    },
+                    parent: 'hellothere-parent',
+                    child: 'hellothere-child'
+                }
+            ]
+        }, {
+            type: actions.DELETE_BOARD
+        })).toEqual({
+            id: '',
+            content: {},
+            columns: []
+        })
+    })
+
+    test('should update card with given id to have new information', () => {
+        expect(reducer({
+            columns: [
+                {
+                    id: 'hello',
+                    content: {
+                        name: 'name'
+                    },
+                    parent: 'hello-parent',
+                    child: 'hello-child',
+                    cards: [
+                        {
+                            id: 'hellocard1',
+                            content: {
+                                text: 'wooo'
+                            },
+                            parent: 'hellothere',
+                            child: 'lalala',
+                            column_header: 'hello'
+                        },
+                        {
+                            id: 'hellocard2',
+                            content: {
+                                text: 'wooo2'
+                            },
+                            parent: 'hellothere2',
+                            child: 'lalala2',
+                            column_header: 'hello'
+                        }
+                    ]
+                }
+            ]
+        }, {
+            type: actions.UPDATE_CARD,
+            card: {
+                id: 'hellocard2',
+                content: {
+                    text: 'wooo-changed'
+                },
+                parent: 'changed',
+                child: 'changed',
+                column_header: 'changed'
+            }
+        })).toEqual({
+            columns: [
+                {
+                    id: 'hello',
+                    content: {
+                        name: 'name'
+                    },
+                    parent: 'hello-parent',
+                    child: 'hello-child',
+                    cards: [
+                        {
+                            id: 'hellocard1',
+                            content: {
+                                text: 'wooo'
+                            },
+                            parent: 'hellothere',
+                            child: 'lalala',
+                            column_header: 'hello'
+                        },
+                        {
+                            id: 'hellocard2',
+                            content: {
+                                text: 'wooo-changed'
+                            },
+                            parent: 'changed',
+                            child: 'changed',
+                            column_header: 'changed'
+                        }
+                    ]
+                }
+            ]
+        })
+    })
+
+    test('should update fetching flags when fetching boards', () => {
+        expect(reducer({}, {
+            type: actions.FETCH_BOARDS
+        })).toEqual({
+            isFetchingBoards: true,
+            boardsError: false
+        })
+    })
+
+    test('should update fetching flags when fetching a board', () => {
+        expect(reducer({}, {
+            type: actions.FETCH_BOARD
+        })).toEqual({
+            isFetchingBoard: true,
+            boardError: false
+        })
+    })
 })
