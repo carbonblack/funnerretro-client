daab9e2ac19e8f0bda452e45c910fe6d52916e76
Jake Hackett <jhackett@carbonblack.com>
2018-05-21 12:00:24 -0400

    starting boards table

--- components/Boards.jsx
+++ components/Boards.jsx
@@ -4,6 +4,7 @@ import FontAwesome from 'react-fontawesome'
 import { Link } from 'react-router-dom'
 import colors from '../styles/colors'
 import { actionButton } from '../styles/button'
+import BoardsTable from './boards/BoardsTable'
 
 const container = css`
     display: flex;
@@ -18,29 +19,6 @@ const inner = css`
     display: flex;
     justify-content: center;
     flex-wrap: wrap;
-`
-
-const boardContainer = css`
-    padding: 0 1rem;
-    margin-bottom: 2rem;
-`
-
-const decoration = css`
-    content: "";
-    position: absolute;
-    top: 0;
-    width: 15px;
-    height: 100%;
-    background: ${ colors.lightPink };
-`
-
-const boardInner = css`
-    display: flex;
-    background: ${ colors.offWhite };
-    border: 4px solid ${ colors.black };
-    padding: 1rem 2rem;
-    border-radius: 2px;
-    position: relative;
 `
 
 const header = css`
@@ -58,19 +36,6 @@ const headerInner = css`
     text-transform: uppercase;
 `
 
-const boardLink = css`
-    color: ${ colors.black };
-
-    &:before {
-        ${ decoration };
-        left: 0;
-    }
-`
-
-const createLink = css`
-    color: ${ colors.pink };
-`
-
 class Boards extends Component {
     componentDidMount() {
         this.props.load()
@@ -83,25 +48,7 @@ class Boards extends Component {
                     <h2 className={ headerInner }>All Retro Boards</h2>
                 </div>
                 <div className={ inner }>
-                    {this.props.boards.length < 1 &&
-                        <div className={ boardContainer }>
-                            <div className={ boardInner }>
-                                <p>There are no boards right now. Be the first to <Link className={ createLink } to="/board/new">create one</Link>!</p>
-                            </div>
-                        </div>
-                    }
-                    {this.props.boards.map(board => (
-                        <div key={ `board-${ board.id }` } className={ boardContainer }>
-                            <div className={ boardInner }>
-                                <Link className={ boardLink } to={`/board/${ board.id }`}>
-                                    <p>{ board.content.name }</p>
-                                </Link>
-                                <button onClick={ () => this.props.onDelete(board.id) } className={ actionButton }>
-                                    <FontAwesome name="trash-o" />
-                                </button>
-                            </div>
-                        </div>
-                    ))}
+                    <BoardsTable boards={ this.props.boards } onDelete={ (id) => this.props.onDelete(id) } />
                 </div>
             </div>
         )
