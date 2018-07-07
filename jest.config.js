37a2d166a41191665522bb82f299ad6da40f95db
Jacob Hackett <jacobrhackett@gmail.com>
2018-07-07 15:27:38 -0400

    using jest.config.js instead of putting the config in package.json

---
+++ jest.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    "collectCoverageFrom": [
+        "**/*.{js,jsx}",
+        "!**/node_modules/**",
+        "!webpack.config.js"
+    ],
+    "moduleNameMapper": {
+        "\\.(png|svg)$": "<rootDir>/testing/fileMock",
+        "^actions/(.*)": "<rootDir>/actions/$1",
+        "^components/(.*)": "<rootDir>/components/$1",
+        "^constants/(.*)": "<rootDir>/constants/$1",
+        "^containers/(.*)": "<rootDir>/containers/$1",
+        "^reducers/(.*)": "<rootDir>/reducers/$1",
+        "^reducers": "<rootDir>/reducers/",
+        "^styles/(.*)": "<rootDir>/styles/$1"
+    },
+    "setupFiles": [
+        "<rootDir>/testing/setup.js"
+    ],
+    "transform": {
+        "^.+\\.js[x]?$": "babel-jest"
+    }
+}
