b49e4eed3cfd2af2b9bb9df05df5f81ebd1114a6
Jacob Hackett <jacobrhackett@gmail.com>
2018-07-07 15:22:54 -0400

    adding in root component tests again and fixing some testing setup

---
+++ components/__tests__/Routes.test.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import Routes from 'components/Routes'
+
+describe('Routes test', () => {
+    it('should render with the required props', () => {
+        const component = <Routes />
+        expect(shallow(component).exists()).toBe(true)
+    })
+})
