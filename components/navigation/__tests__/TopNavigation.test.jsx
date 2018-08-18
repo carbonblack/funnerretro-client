9fee12548fc4f126cec9e154f027713c913814ff
Jacob Hackett <jacobrhackett@gmail.com>
2018-08-18 16:13:37 -0400

    test topnavigations conditional rendering

--- components/navigation/__tests__/TopNavigation.test.jsx
+++ components/navigation/__tests__/TopNavigation.test.jsx
@@ -3,8 +3,13 @@ import { shallow } from 'enzyme'
 import TopNavigation from 'components/navigation/TopNavigation'
 
 describe('TopNavigation test', () => {
-    it('should render with the required props', () => {
-        const component = <TopNavigation />
-        expect(shallow(component).exists()).toBe(true)
+    it('should render null if shouldn\'t show', () => {
+        const component = <TopNavigation shouldShow={ false } />
+        expect(shallow(component).find('div').exists()).toBe(false)
+    })
+
+    it('should render and show the nav', () => {
+        const component = <TopNavigation shouldShow={ true } />
+        expect(shallow(component).find('div').exists()).toBe(true)
     })
 })
