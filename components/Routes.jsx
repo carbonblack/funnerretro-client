b54f66ea18bff5bd2e39e7c4717ae8b236f5cc66
Jake Hackett <jhackett@carbonblack.com>
2018-08-21 18:20:33 -0400

    remove home and redirect to the boards page

--- components/Routes.jsx
+++ components/Routes.jsx
@@ -1,6 +1,5 @@
 import React, { Fragment } from 'react'
-import { Route, Switch } from 'react-router-dom'
-import Home from 'components/Home'
+import { Route, Switch, Redirect } from 'react-router-dom'
 import BoardsContainer from 'containers/BoardsContainer'
 import BoardContainer from 'containers/BoardContainer'
 import BoardFormContainer from 'containers/NewBoardContainer'
@@ -10,7 +9,7 @@ const Routes = () => (
     <Fragment>
         <TopNavigation />
         <Switch>
-            <Route exact path="/" component={ Home } />
+            <Route exact path="/" render={ () => (<Redirect to="/boards/0"/>) } />
             <Route path="/board/new" component={ BoardFormContainer } />
             <Route path="/board/:id" component={ BoardContainer } />
             <Route path="/boards/:id?" component={ BoardsContainer } />
