61958b0da994129270f28ed5a55e8d5873a2675c
Jake Hackett <jhackett@carbonblack.com>
2018-08-23 09:45:14 -0400

    style username

--- components/navigation/TopNavigation.jsx
+++ components/navigation/TopNavigation.jsx
@@ -5,29 +5,40 @@ import { Link } from 'react-router-dom'
 import brandLogo from 'assets/retrospec_logo.png'
 import colors from 'styles/colors'
 
-const nav = css`
-    width: 5.1rem;
-    height: 4.7rem;
-    background-color: ${ colors.white };
-    box-shadow: 0 2px 4px 0 rgba(0,0,0,0.5);
-    margin: 0 2.6rem 1.5rem;
-`
+const styles = {
+    container: css`
+        display: flex;
+        justify-content: space-between;
+        color: ${ colors.white };
+        margin: 0 2.6rem 1.5rem;
+    `,
+    nav: css`
+        width: 5.1rem;
+        height: 4.7rem;
+        background-color: ${ colors.white };
+        box-shadow: 0 2px 4px 0 rgba(0,0,0,0.5);
+    `,
+    brand: css`
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100%;
 
-const brand = css`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 100%;
-
-    img {
-        width: 4rem;
-    }
-`
+        img {
+            width: 4rem;
+        }
+    `,
+    user: css`
+        margin-top: 1rem;
+    `
+}
 
 const TopNavigation = ({ username }) => (
-    <div className={ nav }>
-        <Link className={ brand } to="/"><img src={ brandLogo } /></Link>
-        <p>{ username }</p>
+    <div className={ styles.container }>
+        <div className={ styles.nav }>
+            <Link className={ styles.brand } to="/"><img src={ brandLogo } /></Link>
+        </div>
+        <p className={ styles.user }>{ username }</p>
     </div>
 )
 
