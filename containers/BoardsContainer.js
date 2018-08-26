03bd076ee3e1d023034c0a409f3622bd4b6d2719
jlorusso <jlorusso@carbonblack.com>
2018-08-26 17:26:13 -0400

    Ensure workspace name is shown as the header on the boards page

--- containers/BoardsContainer.js
+++ containers/BoardsContainer.js
@@ -3,12 +3,18 @@ import { getBoards, deleteBoard, toggleEditingBoards } from 'actions/board'
 import Boards from 'components/boards/Boards'
 import { updateBoardsSearchDefinition } from 'actions/board'
 
-const mapStateToProps = (state, ownProps) => ({
-    boards: state.board.boards,
-    isFetchingBoards: state.board.isFetchingBoards,
-    isEditingBoards: state.board.isEditingBoards,
-    key: ownProps.match.params.id
-})
+const mapStateToProps = (state, ownProps) => {
+    const groupId = ownProps.match.params.id
+    const group = groupId !== '0' ? state.groups.groups.find(g => g.id === groupId) : { name: 'My Boards' }
+
+    return {
+        boards: state.board.boards,
+        isFetchingBoards: state.board.isFetchingBoards,
+        isEditingBoards: state.board.isEditingBoards,
+        key: groupId,
+        group: group
+    }
+}
 
 const mapDispatchToProps = (dispatch, ownProps) => ({
     load: (sortKey, sortDirection) => {
